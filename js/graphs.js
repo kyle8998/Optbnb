507c0de6029ee8d7da03a7a431f6b4d0ff87760a
Kyle Lim <kylelim8998@gmail.com>
2017-11-07 23:30:48 -0500

    Fixed Javascript Object Overflow and Added Radar Graph
    Graph objects were be instantiated but never destroyed. Global variables
    in graph.js added to account for this issue. Radar graph to represent
    the ratings added in replacement of the regression graph.

--- js/graphs.js
+++ js/graphs.js
@@ -1,12 +1,22 @@
+// Set bar graph variables to destroy and reanimate
+var graph1 = null;
+var graph2 = null;
+var graph3 = null;
+var graph4 = null;
 
-var graph1 = {};
-var graph2 = {};
-var graph3 = {};
-var graph4 = {};
-
+Papa.parse("neighbourhoods.csv", {
+	download: true,
+	complete: function(results) {
+		console.log(results);
+	}
+});
 
 showgraph1();
+
 function showgraph1() {
+    if (graph2 != null) graph2.destroy();
+    if (graph3 != null) graph3.destroy();
+    if (graph4 != null) graph4.destroy();
     var a = document.getElementById("1room");
     var b = document.getElementById("2room");
     var c = document.getElementById("3room");
@@ -17,10 +27,8 @@ function showgraph1() {
         c.style.display = "none";
         d.style.display = "none";
 
-    // graph1.destroy()
-
     var ctx = document.getElementById("col_bar").getContext('2d');
-    var col_bar = new Chart(ctx, {
+    graph1 = new Chart(ctx, {
         type: 'horizontalBar',
         data: {
             labels: ["Bayview","Bernal Heights" ,"Castro/Upper Market" ,"Chinatown" ,"Crocker Amazon" ,"Diamond Heights" ,"Downtown/Civic Center" ,"Excelsior" ,"Financial District" ,"Glen Park" ,"Golden Gate Park" ,"Haight Ashbury" ,"Inner Richmond" ,"Inner Sunset" ,"Lakeshore" ,"Marina" ,"Mission" ,"Nob Hill" ,"Noe Valley" ,"North Beach" ,"Ocean View" ,"Outer Mission" ,"Outer Richmond" ,"Outer Sunset" ,"Pacific Heights" ,"Parkside" ,"Potrero Hill" ,"Presidio" ,"Presidio Heights" ,"Russian Hill" ,"Seacliff" ,"South of Market" ,"Treasure Island/YBI" ,"Twin Peaks" ,"Visitacion Valley" ,"West of Twin Peaks" ,"Western Addition"],
@@ -68,6 +76,9 @@ function showgraph1() {
 
 }
 function showgraph2() {
+    if (graph1 != null) graph1.destroy();
+    if (graph3 != null) graph3.destroy();
+    if (graph4 != null) graph4.destroy();
     var a = document.getElementById("2room");
     var b = document.getElementById("1room");
     var c = document.getElementById("3room");
@@ -79,7 +90,7 @@ function showgraph2() {
         d.style.display = "none";
 
     var ctx2 = document.getElementById("col_bar2").getContext('2d');
-    var col_bar2 = new Chart(ctx2, {
+    graph2 = new Chart(ctx2, {
         type: 'horizontalBar',
         data: {
             labels: ["Bayview","Bernal Heights" ,"Castro/Upper Market" ,"Chinatown" ,"Crocker Amazon" ,"Diamond Heights" ,"Downtown/Civic Center" ,"Excelsior" ,"Financial District" ,"Glen Park" ,"Golden Gate Park" ,"Haight Ashbury" ,"Inner Richmond" ,"Inner Sunset" ,"Lakeshore" ,"Marina" ,"Mission" ,"Nob Hill" ,"Noe Valley" ,"North Beach" ,"Ocean View" ,"Outer Mission" ,"Outer Richmond" ,"Outer Sunset" ,"Pacific Heights" ,"Parkside" ,"Potrero Hill" ,"Presidio" ,"Presidio Heights" ,"Russian Hill" ,"Seacliff" ,"South of Market" ,"Treasure Island/YBI" ,"Twin Peaks" ,"Visitacion Valley" ,"West of Twin Peaks" ,"Western Addition"],
@@ -127,6 +138,9 @@ function showgraph2() {
 
 }
 function showgraph3() {
+    if (graph1 != null) graph1.destroy();
+    if (graph2 != null) graph2.destroy();
+    if (graph4 != null) graph4.destroy();
     var a = document.getElementById("3room");
     var b = document.getElementById("1room");
     var c = document.getElementById("2room");
@@ -138,7 +152,7 @@ function showgraph3() {
         d.style.display = "none";
 
     var ctx3 = document.getElementById("col_bar3").getContext('2d');
-    var col_bar3 = new Chart(ctx3, {
+    graph3 = new Chart(ctx3, {
         type: 'horizontalBar',
         data: {
             labels: ["Bayview","Bernal Heights" ,"Castro/Upper Market" ,"Chinatown" ,"Crocker Amazon" ,"Diamond Heights" ,"Downtown/Civic Center" ,"Excelsior" ,"Financial District" ,"Glen Park" ,"Golden Gate Park" ,"Haight Ashbury" ,"Inner Richmond" ,"Inner Sunset" ,"Lakeshore" ,"Marina" ,"Mission" ,"Nob Hill" ,"Noe Valley" ,"North Beach" ,"Ocean View" ,"Outer Mission" ,"Outer Richmond" ,"Outer Sunset" ,"Pacific Heights" ,"Parkside" ,"Potrero Hill" ,"Presidio" ,"Presidio Heights" ,"Russian Hill" ,"Seacliff" ,"South of Market" ,"Treasure Island/YBI" ,"Twin Peaks" ,"Visitacion Valley" ,"West of Twin Peaks" ,"Western Addition"],
@@ -186,6 +200,9 @@ function showgraph3() {
 
 }
 function showgraph4() {
+    if (graph1 != null) graph1.destroy();
+    if (graph2 != null) graph2.destroy();
+    if (graph3 != null) graph3.destroy();
     var a = document.getElementById("4room");
     var b = document.getElementById("1room");
     var c = document.getElementById("2room");
@@ -197,7 +214,7 @@ function showgraph4() {
         d.style.display = "none";
 
     var ctx4 = document.getElementById("col_bar4").getContext('2d');
-    var col_bar4 = new Chart(ctx4, {
+    graph4 = new Chart(ctx4, {
         type: 'horizontalBar',
         data: {
             labels: ["Bayview","Bernal Heights" ,"Castro/Upper Market" ,"Chinatown" ,"Crocker Amazon" ,"Diamond Heights" ,"Downtown/Civic Center" ,"Excelsior" ,"Financial District" ,"Glen Park" ,"Golden Gate Park" ,"Haight Ashbury" ,"Inner Richmond" ,"Inner Sunset" ,"Lakeshore" ,"Marina" ,"Mission" ,"Nob Hill" ,"Noe Valley" ,"North Beach" ,"Ocean View" ,"Outer Mission" ,"Outer Richmond" ,"Outer Sunset" ,"Pacific Heights" ,"Parkside" ,"Potrero Hill" ,"Presidio" ,"Presidio Heights" ,"Russian Hill" ,"Seacliff" ,"South of Market" ,"Treasure Island/YBI" ,"Twin Peaks" ,"Visitacion Valley" ,"West of Twin Peaks" ,"Western Addition"],
@@ -249,116 +266,117 @@ function showgraph4() {
 
 
 // Scatter plot!
-var scatter = document.getElementById('scatter');
-
-var scatter_chart = new Chart(scatter, {
-  type: 'line',
-  data: {
-    labels: makeLabels().labels,
-    datasets: [{
-      type: 'line',
-      label: 'Data',
-      data: makeLabels().labels,
-      fill: false,
-      backgroundColor: "rgba(218,83,79, .7)",
-      borderColor: "rgba(218,83,79, .7)",
-      pointRadius: 0
-    }, {
-      type: 'bubble',
-      label: 'Data2',
-      data: makeBubbles(),
-      backgroundColor: "rgba(76,78,80, .7)",
-      borderColor: "transparent"
-    }]
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        type: 'linear',
-        position: 'bottom',
-        ticks: {
-          autoSkip: true,
-          max: Math.max(...makeLabels().array)
-        }
-      }]
-    }
-  }
-});
-
-function makeLabels() {
-  let arr = [
-
-    1,
-
-    100
-  ];
-
-  arr = arr.sort((a, b) => a - b);
-  let newarr = arr.map(item => ({ x: item, y: item }));
-  return {
-    labels: newarr,
-    array: arr
-  };
-};
-
-function makeBubbles() {
-  let arr = [
-    1,
-    2,
-    8.501876487304344,
-    6.63200177739563,
-    6.815639990074331,
-    9.455871442168021,
-    10.745464137157782,
-    6.293419278846481,
-    10.559036702680755,
-    6.371611847231857,
-    9.472242991440476,
-    9.356084386028888,
-    6.761572768804055,
-    9.556409270127691,
-    9.331052447138447,
-    10.291569105033412,
-    6.018593214496234,
-    6.354370040797351,
-    10.302599186672117,
-    6.326149473155099,
-    10.274947655341608,
-    8.635331719433278,
-    10.425757440186377,
-    8.760923376338836,
-    6.345636360828596,
-    9.974784434418162,
-    10.486149999920801,
-    6.054439346269371,
-    10.963185747705012,
-    6.049733455231958,
-    10.429960121878297,
-    5.857933154483459,
-    6.368187186350492,
-    9.474625866621567,
-    10.878160393673937,
-    10.696480068065789,
-    9.838949031398556,
-    7.050989447068045,
-    11.932378944680496,
-    6.489204931325317,
-    5.564520407322694,
-    5.849324779946859,
-    6.144185634125646,
-    9.550662331752143,
-    5.8971538676367405,
-    5.855071922202427,
-    7.586803535162581,
-    10.55664572487971
-  ];
-
-  arr = arr.map(function(item, i) {
-    return {x: item, y:arr[i - 1]}
-  });
-
-  return arr;
-};
+// var scatter = document.getElementById('scatter');
+//
+// var scatter_chart = new Chart(scatter, {
+//   type: 'line',
+//   data: {
+//     labels: makeLabels().labels,
+//     datasets: [{
+//       type: 'line',
+//       label: 'Data',
+//       data: makeLabels().labels,
+//       fill: false,
+//       backgroundColor: "rgba(218,83,79, .7)",
+//       borderColor: "rgba(218,83,79, .7)",
+//       pointRadius: 0
+//     }, {
+//       type: 'bubble',
+//       label: 'Data2',
+//       data: makeBubbles(),
+//       backgroundColor: "rgba(76,78,80, .7)",
+//       borderColor: "transparent"
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       xAxes: [{
+//         type: 'linear',
+//         position: 'bottom',
+//         ticks: {
+//           autoSkip: true,
+//           max: Math.max(...makeLabels().array)
+//         }
+//       }]
+//     }
+//   }
+// });
+//
+// function makeLabels() {
+//   let arr = [
+//
+//     1,
+//
+//     100
+//   ];
+//
+//   arr = arr.sort((a, b) => a - b);
+//   let newarr = arr.map(item => ({ x: item, y: item }));
+//   return {
+//     labels: newarr,
+//     array: arr
+//   };
+// };
+//
+// function makeBubbles() {
+//   let arr = [
+//     1,
+//     2,
+//     8.501876487304344,
+//     6.63200177739563,
+//     6.815639990074331,
+//     9.455871442168021,
+//     10.745464137157782,
+//     6.293419278846481,
+//     10.559036702680755,
+//     6.371611847231857,
+//     9.472242991440476,
+//     9.356084386028888,
+//     6.761572768804055,
+//     9.556409270127691,
+//     9.331052447138447,
+//     10.291569105033412,
+//     6.018593214496234,
+//     6.354370040797351,
+//     10.302599186672117,
+//     6.326149473155099,
+//     10.274947655341608,
+//     8.635331719433278,
+//     10.425757440186377,
+//     8.760923376338836,
+//     6.345636360828596,
+//     9.974784434418162,
+//     10.486149999920801,
+//     6.054439346269371,
+//     10.963185747705012,
+//     6.049733455231958,
+//     10.429960121878297,
+//     5.857933154483459,
+//     6.368187186350492,
+//     9.474625866621567,
+//     10.878160393673937,
+//     10.696480068065789,
+//     9.838949031398556,
+//     7.050989447068045,
+//     11.932378944680496,
+//     6.489204931325317,
+//     5.564520407322694,
+//     5.849324779946859,
+//     6.144185634125646,
+//     9.550662331752143,
+//     5.8971538676367405,
+//     5.855071922202427,
+//     7.586803535162581,
+//     10.55664572487971
+//   ];
+//
+//   arr = arr.map(function(item, i) {
+//     return {x: item, y:arr[i - 1]}
+//   });
+//
+//   return arr;
+// };
+//
 
 
 
@@ -398,4 +416,23 @@ new Chart(document.getElementById("line-chart"), {
       text: 'Bedroom to Bathroom Price Ratio'
     }
   }
+});
+
+
+
+// Radar graph
+var radarCanvas = document.getElementById("radar_chart");
+
+var radarData = {
+  labels: ["Accuracy", "Cleanliness", "Check-in", "Communication", "Location", "Value"],
+  datasets: [{
+    label: "Bayview",
+    backgroundColor: "rgba(54, 162, 235,0.2)",
+    data: [9.425531915,9.29787234,9.755319149,9.712765957,8.159574468,9.14893617]
+}]
+};
+
+var radarChart = new Chart(radarCanvas, {
+  type: 'radar',
+  data: radarData
 });
